cfb79ef9c06ddafb0d2a43e7379ef2283294223c
kentmartin12 <kentmartin12@gmail.com>
2021-01-08 18:32:30 +0700

    login.blade done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,9 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="container mt-4 d-flex justify-content-between">
+    <div>
+        <div>
+            <h1 style="font-size: 68px;">Donasi Untuk</h1>
+            <h1 class="mb-5" style="color: #0253B3; line-height: 30px; font-size: 68px;">Lembiru.id</h1>
+        </div>
+
+        <p style="font-size: 24px;">Dukung kami untuk terus menghubungkan orang-orang <br>
+            baik di seluruh Indonesia, serta untuk menggerakkan proses <br> 
+            digitalisasi Indonesia.</p>
+        <div class="d-flex" style="justify-content: flex-end">
+            <img src="{{ asset('asset/regisVector.png') }}" alt="" style="width: 300px; height: 212px;">
+        </div>
+    </div>
+
+    <div class="row justify-content-end">
+        {{-- <div class="col">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
 
@@ -67,6 +81,67 @@
                     </form>
                 </div>
             </div>
+        </div> --}}
+
+        <div class="d-flex align-items-center">
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <div class="form-group row">
+                    <div class="col">
+                        <input style="background-color: #EFF2F4; border-color: #EFF2F4; width: 25vw; height: 3vw; padding: 25px;" placeholder="Email address" id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <div class="col">
+                        <input style="background-color: #EFF2F4; border-color: #EFF2F4; width: 25vw; height: 3vw; padding: 25px;" placeholder="Password" id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                        @error('password')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
+                    </div>
+                </div>
+
+                <button class="btn-lg mt-3" style="background-color: #0253B3; width: 212px; color: #FFFFFF; border-color:#0253B3; width: 25vw;">
+                    Masuk
+                </button>
+
+                <div class="text-center mt-4">
+                    <p>atau dengan</p>
+                </div>
+
+                <div class="d-flex justify-content-between mt-4">
+                    <span class="border" style="border-radius: 10px; padding: 10px 20px;">
+                        <a href="">
+                            <img src="{{ asset('asset/logoGoogle.png') }}" alt="" style="margin-top: 5px;">
+                        </a>
+                    </span>
+
+                    <span class="border" style="border-radius: 10px; padding: 10px 20px;">
+                        <a href="">
+                            <img src="{{ asset('asset/logoApple.png') }}" alt="">
+                        </a>
+                    </span>
+
+                    <span class="border" style="border-radius: 10px; padding: 10px 20px;">
+                        <a href="">
+                            <img src="{{ asset('asset/logoFb.png') }}" alt="">
+                        </a>
+                    </span>
+                </div>
+
+                <div class="d-flex mt-4 justify-content-center">
+                    <p>Belum punya akun?&nbsp</p>
+                    <a href="{{ route('register') }}"><p>Registrasi di sini.</p></a>
+                </div>
+            </form>
         </div>
     </div>
 </div>
